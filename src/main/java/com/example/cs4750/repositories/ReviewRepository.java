7dec587f938e4311bb10747d6dcbf338b92cf244
ethan <mud5xz@virginia.edu>
2024-12-02 19:24:17 -0500

    test commit

--- src/main/java/com/example/cs4750/repositories/ReviewRepository.java
+++ src/main/java/com/example/cs4750/repositories/ReviewRepository.java
@@ -14,4 +14,5 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
     @Query("SELECT MAX(r.reviewId) FROM Review r")
     Optional<Integer> findMaxReviewId();
+    //test
 }
